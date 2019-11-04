0807b6498f97ad4f70a9991a71d933881dc16f41
Daniela Hernandez <dannyhvalenz@live.com.mx>
2019-11-04 15:35:54 -0600

    actualizacion paint.js

--- Paint/js/paint.js
+++ Paint/js/paint.js
@@ -18,10 +18,10 @@ var pinturaCamara = false;
 var anteriorX, anteriorY = 0;
 
 const modelParams = {
-    flipHorizontal: true, // flip e.g for video  
     maxNumBoxes: 1, // maximum number of boxes to detect
     iouThreshold: 0.5, // ioU threshold for non-max suppression
     scoreThreshold: 0.6, // confidence threshold for predictions.
+    flipHorizontal: true,
 }
 
 function startVideo() {
